f4b0804b93ff3f1a6bc0037ca66c1027029e0ef4
{김병창} <{kb_6537@naver.com}>
2023-09-04 17:45:50 +0900

    modified:   Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,12 @@
 public class Calculator {
+    public double Division(int a, int b){
+        
+        double answer = 0;
+
+        answer = (double)(a / b);
+        
+        return answer;
+    
+    }
 
 }
